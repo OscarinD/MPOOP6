608177a26fc7fd7b37426748595da5872c6fd3b8
OscarinD <102172290+OscarinD@users.noreply.github.com>
2022-03-22 21:36:59 -0600

    Práctica 6. Carpeta source

---
+++ src/controlador/Sumadora.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+/**
+ *
+ * @author 52557
+ */
+//Clase que suma números
+public class Sumadora {
+    /**Constructor vacío
+*/
+    public Sumadora(){
+    }
+/**
+* Método que realiza una suma de dos números
+* @param a El primer número a sumar
+* @param b El segundo númoer a sumar
+* @return La suma de dos valores double
+*/
+    public double sumar(double a, double b){
+    double suma = a + b;
+    return suma;
+    }
+}
